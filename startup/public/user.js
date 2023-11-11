f72eef53be40087ed6597b90ee151d7505ba552c
jawy1248 <112504611+jawy1248@users.noreply.github.com>
2023-11-11 15:54:51 -0700

    Final touches

--- startup/public/user.js
+++ startup/public/user.js
@@ -3,37 +3,37 @@ async function loadSearches() {
     let pic1HTML =  
     '<div class="col d-flex flex-column m-3">' +
         '<span class="h5">Furniture Set 1</span>' +
-        '<img class="m-1 rounded-3" src="images/furn/furn1.jpeg" alt="Furniture Set 1" height="250" />' +
+        '<div class="text-center"><img class="m-1 rounded-3" src="images/furn/furn1.jpeg" alt="Furniture Set 1" width="300" /></div>' +
     '</div>';
     
     let pic2HTML = 
     '<div class="col d-flex flex-column m-3">' +
         '<span class="h5">Furniture Set 2</span>' +
-        '<img class="m-1 rounded-3" src="images/furn/furn2.jpg" alt="Furniture Set 2" height="250" />' +
+        '<div class="text-center"><img class="m-1 rounded-3" src="images/furn/furn2.jpg" alt="Furniture Set 2" width="300" /></div>' +
     '</div>';
     
     let pic3HTML = 
     '<div class="col d-flex flex-column m-3">' +
         '<span class="h5">Furniture Set 3</span>' +
-        '<img class="m-1 rounded-3" src="images/furn/furn3.jpg" alt="Furniture Set 3" height="250" />' +
+        '<div class="text-center"><img class="m-1 rounded-3" src="images/furn/furn3.jpg" alt="Furniture Set 3" width="300" /></div>' +
     '</div>';
     
     let pic4HTML = 
     '<div class="col d-flex flex-column m-3">' +
         '<span class="h5">Furniture Set 4</span>' +
-        '<img class="m-1 rounded-3" src="images/furn/furn4.jpg" alt="Furniture Set 4" height="250" />' +
+        '<div class="text-center"><img class="m-1 rounded-3" src="images/furn/furn4.jpg" alt="Furniture Set 4" width="300" /></div>' +
     '</div>';
     
     let pic5HTML = 
     '<div class="col d-flex flex-column m-3">' +
         '<span class="h5">Furniture Set 5</span>' +
-        '<img class="m-1 rounded-3" src="images/furn/furn5.jpg" alt="Furniture Set 5" height="250" />' +
+        '<div class="text-center"><img class="m-1 rounded-3" src="images/furn/furn5.jpg" alt="Furniture Set 5" width="300" /></div>' +
     '</div>';
     
     let pic6HTML = 
     '<div class="col d-flex flex-column m-3">' +
         '<span class="h5">Furniture Set 6</span>' +
-        '<img class="m-1 rounded-3" src="images/furn/furn6.jpg" alt="Furniture Set 6" height="250" />' +
+        '<div class="text-center"><img class="m-1 rounded-3" src="images/furn/furn6.jpg" alt="Furniture Set 6" width="300" /></div>' +
     '</div>';
     
     let picHTML = [pic1HTML, pic2HTML, pic3HTML, pic4HTML, pic5HTML, pic6HTML];
