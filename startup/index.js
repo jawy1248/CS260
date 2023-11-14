c4baaa5522462336f393c4994c6a39c2ecc76d64
jawy1248 <112504611+jawy1248@users.noreply.github.com>
2023-11-13 22:01:42 -0700

    Added Mango, finished assignment

--- startup/index.js
+++ startup/index.js
@@ -1,7 +1,17 @@
 // Require Mango
 const { MongoClient } = require('mongodb');
-const url = `mongodb+srv://cs260:cs260password@cluster0.cocupz0.mongodb.net/`;
+const config = require("./dbConfig.json")
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}/`;
 const client = new MongoClient(url);
+
+// Test connection
+(async function testConnection() {
+    await client.connect();
+    await db.command({ ping: 1 });
+  })().catch((ex) => {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  });
 
 // Require Express
 const express = require('express');
