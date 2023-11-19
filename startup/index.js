d544a33dc80c6fabb5ca8e6215d941058d2966cd
jawy1248 <112504611+jawy1248@users.noreply.github.com>
2023-11-18 19:21:17 -0700

    Got the screen to change when logges in

--- startup/index.js
+++ startup/index.js
@@ -32,62 +32,66 @@ app.use(`/api`, apiRouter);
 // CreateAuth token for a new user
 apiRouter.post('/auth/create', async (req, res) => {
     if (await DB.getUser(req.body.email)) {
-      res.status(409).send({ msg: 'Existing user' });
+        res.status(409).send({ msg: 'Existing user' });
+        console.log("Unable to create");
     } else {
-      const user = await DB.createUser(req.body.email, req.body.password);
-  
-      // Set the cookie
-      setAuthCookie(res, user.token);
-  
-      res.send({
-        id: user._id,
-      });
-    }
-  });
+        const user = await DB.createUser(req.body.email, req.body.password);
 
-  // GetAuth token for the provided credentials
-  apiRouter.post('/auth/login', async (req, res) => {
+        // Set the cookie
+        setAuthCookie(res, user.token);
+
+        res.send({
+            id: user._id,
+        });
+        console.log("Successfully created an account");
+    }
+});
+
+// GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
     const user = await DB.getUser(req.body.email);
     if (user) {
-      if (await bcrypt.compare(req.body.password, user.password)) {
+        if (await bcrypt.compare(req.body.password, user.password)) {
         setAuthCookie(res, user.token);
         res.send({ id: user._id });
+        console.log("Successfully logged in");
         return;
-      }
+        }
     }
     res.status(401).send({ msg: 'Unauthorized' });
-  });
+    console.log("Unable to login");
+});
 
-  // DeleteAuth token if stored in cookie
-  apiRouter.delete('/auth/logout', (_req, res) => {
+// DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
     res.clearCookie(authCookieName);
     res.status(204).end();
-  });
+});
 
-  // GetUser returns information about a user
-  apiRouter.get('/user/:email', async (req, res) => {
+// GetUser returns information about a user
+apiRouter.get('/user/:email', async (req, res) => {
     const user = await DB.getUser(req.params.email);
     if (user) {
-      const token = req?.cookies.token;
-      res.send({ email: user.email, authenticated: token === user.token });
-      return;
+        const token = req?.cookies.token;
+        res.send({ email: user.email, authenticated: token === user.token });
+        return;
     }
     res.status(404).send({ msg: 'Unknown' });
-  });
+});
 
-  // secureApiRouter verifies credentials for endpoints
-  var secureApiRouter = express.Router();
-  apiRouter.use(secureApiRouter);
-  
-  secureApiRouter.use(async (req, res, next) => {
-    authToken = req.cookies[authCookieName];
-    const user = await DB.getUserByToken(authToken);
+// secureApiRouter verifies credentials for endpoints
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+authToken = req.cookies[authCookieName];
+const user = await DB.getUserByToken(authToken);
     if (user) {
-      next();
+        next();
     } else {
-      res.status(401).send({ msg: 'Unauthorized' });
+        res.status(401).send({ msg: 'Unauthorized' });
     }
-  });
+});
 
 // -------------- Save Search --------------
 // print out to make sure we are in
