c32228dd960c97f1ac756a2ed149b071a13bf62c
jawy1248 <112504611+jawy1248@users.noreply.github.com>
2023-12-04 15:05:39 -0700

    Added websocket structure to chat.js

--- startup/public/chat.js
+++ startup/public/chat.js
@@ -14,3 +14,38 @@ function reply(){
     replyID.innerHTML += '\nBot: <pre>This is my reply</pre>';
 }
 
+
+function configureWebSocket() {
+    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+    this.socket.onopen = (event) => {
+        this.displayMsg('system', 'chat', 'connected');
+    };
+    this.socket.onclose = (event) => {
+        this.displayMsg('system', 'chat', 'disconnected');
+    };
+    this.socket.onmessage = async (event) => {
+        const msg = JSON.parse(await event.data.text());
+
+        if (msg.type === GameEndEvent) {
+        this.displayMsg('player', msg.from, `scored ${msg.value.score}`);
+        } else if (msg.type === GameStartEvent) {
+        this.displayMsg('player', msg.from, `started a new game`);
+        }
+    };
+}
+
+function broadcastEvent(from, type, value) {
+    const event = {
+        from: from,
+        type: type,
+        value: value,
+    };
+    this.socket.send(JSON.stringify(event));
+}
+
+function displayMsg(cls, from, msg) {
+    const chatText = document.querySelector('#player-messages');
+    chatText.innerHTML =
+        `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` + chatText.innerHTML;
+}
