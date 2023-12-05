3ced5aedf37c238c8578368896024dee58741ee7
jawy1248 <112504611+jawy1248@users.noreply.github.com>
2023-12-04 17:52:00 -0700

    Working WS

--- startup/public/chat.js
+++ startup/public/chat.js
@@ -1,3 +1,5 @@
+let chatSocket;
+
 function chat(){
     let usernameText = localStorage.getItem('userName');
     const chatID = document.getElementById("chat");
@@ -5,58 +7,61 @@ function chat(){
 
     const chatBackID = document.getElementById("chatBack");
     chatBackID.innerHTML = '<input id="replyInput"></input> <button onclick="reply()">Reply</button>';
-    configureWebSocket();
+    chatSocket = new Socket();
 }
 
 function reply(){
     let usernameText = localStorage.getItem('userName');
-    const replyID = document.getElementById("reply");
-    const replyInput = document.getElementById("replyInput");
-    replyID.innerHTML += '\n' + usernameText + ': <pre>' + replyInput.value + '</pre>';
-    replyID.innerHTML += '\n' + usernameText + ': <pre>This is my reply</pre>';
+    let replyInput = document.getElementById("replyInput");
+    chatSocket.broadcastEvent(usernameText, replyInput?.value);
 }
 
+class Socket {
+    socket;
 
-function configureWebSocket() {
-    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
-    let socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
-    socket.onopen = (event) => {
-        displayMsg('chat', 'connected');
-    };
-    socket.onclose = (event) => {
-        this.displayMsg('chat', 'disconnected');
-    };
-    socket.onmessage = async (event) => {
-        const msg = JSON.parse(await event.data.text());
+    constructor(){
+        this.configureWebSocket();
+    }
 
-        // if (msg.type === GameEndEvent) {
-        //     displayMsg('player', msg.from, `scored ${msg.value.score}`);
-        // } else if (msg.type === GameStartEvent) {
-        //     displayMsg('player', msg.from, `started a new game`);
-        // }
-    };
-}
+    configureWebSocket() {
+        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+        this.socket.onopen = (event) => {
+            this.displayMsg('chat', 'connected');
+        };
+        this.socket.onclose = (event) => {
+            this.displayMsg('chat', 'disconnected');
+        };
+        this.socket.onmessage = async (event) => {
+            const msg = JSON.parse(await event.data.text());
+            this.displayMsg(msg.from, msg.chat);
+        };
+    }
 
-function broadcastEvent(from, chat) {
-    const event = {
-        from: from,
-        chat: type,
-    };
-    this.socket.send(JSON.stringify(event));
-}
+    broadcastEvent(fromIN, chatIN) {
+        const event = {
+            from: fromIN,
+            chat: chatIN,
+        };
+        const chatText = document.getElementById("reply");
+        chatText.innerHTML += '\n' + fromIN + ': <pre>' + chatIN + '</pre>';
 
-function displayMsg(from, msg) {
-    const chatText = document.getElementById("reply");
-    if(from === 'chat'){
-        if(msg === 'connected'){
-            chatText.innerHTML += 'Starting a new conversation...';
-            chatText.innerHTML += '\n';
+        this.socket.send(JSON.stringify(event));
+    }
+
+    displayMsg(from, msg) {
+        const chatText = document.getElementById("reply");
+        if(from === 'chat'){
+            if(msg === 'connected'){
+                chatText.innerHTML += 'Starting a new conversation...';
+                chatText.innerHTML += '\n';
+            }
+            else{
+                chatText.innerHTML += 'Ending the conversation...thank you!';
+            }
         }
         else{
-            chatText.innerHTML += 'Ending the conversation...thank you!';
+            chatText.innerHTML += '\n' + from + ': <pre>' + msg + '</pre>';
         }
-    }
-    else{
-        replyID.innerHTML += '\n' + from + ': <pre>' + msg + '</pre>';
     }
 }
